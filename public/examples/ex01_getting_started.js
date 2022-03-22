01e6c1a9aa8d3244638bbb7097ba78c5f04d2e27
drsanti <santi.inc.kmutt@gmail.com>
2022-03-22 13:24:13 +0700

    update

--- public/examples/ex01_getting_started.js
+++ public/examples/ex01_getting_started.js
@@ -1,4 +1,16 @@
 /**
+ * 01 - GETTING STARTED
+ * 
+ * Graphics and Physics Engine (GPEngin) for Web3D Applications
+ * Asst.Prof.Dr.Santi Nuratch
+ * Embedded Computing and Control Laboratory | ECC-Lab, INC-KMUTT
+ * 
+ * Description: Example of using the GPEngin
+ * Update: 22 March 2022
+ */
+
+
+/**
  * GPEngine
  */
 const Engine = GPEngine;
@@ -12,26 +24,27 @@ const EngineCore = Engine.EngineCore;
 
 
 /**
- * Environment Map
+ * Environment Map/Skybox
  */
-const ENV_MAP = 'pack';
+const ENV_MAP = 'sky';                      // 'bridge', 'canary', 'park', 'sky', 'snow'
 
 
 /**
  * Initial Models
  */
-MODELS = ['assets/models/test.gltf'];
+MODELS = ['assets/models/startup.gltf'];   // glTF models
 
 
 /**
  * Create Engine
  */
-const engine = new EngineCore();
+const engine = new EngineCore({ graphics: { envMap: 'snow' }, physics: { enable: true } });
+
 
 /**
  * Initialize Engine
  */
-engine.init({ envMap: 'park', models: MODELS }).then((args) => {
+engine.init({ models: MODELS }).then((args) => {
     init(args);                 // Call init function to initialize other stuffs 
     args.engine.start(loop);    // Start the engine
 });
@@ -40,14 +53,16 @@ engine.init({ envMap: 'park', models: MODELS }).then((args) => {
 /**
  * Initialize
  */
-function init(args) {
-    console.dir(args);
+function init(iargs) {          // iargs = {engine, graphics, physics, models}    
+    console.dir(iargs);
 }
 
 
 /**
  * Engine Loop
  */
-function loop(args) {
-    console.dir(args);
+function loop(eargs) {          // iargs = {engine, graphics, physics, frameCount, deltaTime}    
+    if (eargs.frameCount % 100 == 0) {
+        console.log(eargs);
+    }
 }
